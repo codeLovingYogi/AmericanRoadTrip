3dbd4d07884df76a1b0dd7096c7382b70b5c55f9
Johnny <felichay@outlook.com>
2016-08-23 15:32:00 -0400

    Plot roadtrip stops on map using Folium

--- roadtripmap.py
+++ roadtripmap.py
@@ -1,16 +1,29 @@
 import csv
+import math
+import folium
 
 # get file name
 fname = input('Enter file name: ')
 if(len(fname) < 1):
-	fname = 'placestomap.csv'
+    fname = 'placestomap.csv'
 
 # open and read file
 with open(fname) as f:
-	reader = csv.reader(f)
-	locations = list(reader)
+    reader = csv.reader(f)
+    locations = list(reader)
 
-for each in locations:
-	print(each)
+n = len(locations) 
+middle = math.floor(n/2)
 
-print('f closed', f.closed)
+tripmap = folium.Map(location=[locations[middle][5], locations[middle][6]],
+                   zoom_start=5,
+                   tiles='Stamen Terrain')
+
+for i in range(1, n):
+    folium.Marker([locations[i][5], locations[i][6]],
+              popup=locations[i][4],
+             ).add_to(tripmap)
+
+tripmap.save('roadtripmap.html')
+
+
